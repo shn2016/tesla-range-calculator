1f930731dd028afbc3fe01d6d18dc496088303b4
Kai GAO <kaigaoinau@gmail.com>
2019-04-11 18:06:39 +1000

    Updates

---
+++ precache-manifest.3cb29967f4da1519e085ea5bab38deb9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "362e471dc23b66a047c7",
+    "url": "/tesla-range-calculator/static/css/main.30e33d9e.chunk.css"
+  },
+  {
+    "revision": "362e471dc23b66a047c7",
+    "url": "/tesla-range-calculator/static/js/main.48bff8e3.chunk.js"
+  },
+  {
+    "revision": "50b2b212f6c048d45344",
+    "url": "/tesla-range-calculator/static/js/runtime~main.87a06476.js"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/tesla-range-calculator/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "99c8f2040d559a70d47006716713add5",
+    "url": "/tesla-range-calculator/static/media/logo.99c8f204.svg"
+  },
+  {
+    "revision": "e787dc928af0297e0c30ebf60ca1e0df",
+    "url": "/tesla-range-calculator/static/media/wheel-21.e787dc92.png"
+  },
+  {
+    "revision": "3b975a003195f4fbb447ee79965fee91",
+    "url": "/tesla-range-calculator/static/media/wheel-19.3b975a00.png"
+  },
+  {
+    "revision": "ff16203f3d0aa6c8a55315d4e3e7b287",
+    "url": "/tesla-range-calculator/static/media/tesla.ff16203f.jpg"
+  },
+  {
+    "revision": "7ee66eea8c5fcc82180de8776e3e37ce",
+    "url": "/tesla-range-calculator/static/media/60.7ee66eea.svg"
+  },
+  {
+    "revision": "246b5ba5f8b8af340199831b3a4b538b",
+    "url": "/tesla-range-calculator/static/media/60d.246b5ba5.svg"
+  },
+  {
+    "revision": "37862a62ee2074b44d66b00fb9c1ebce",
+    "url": "/tesla-range-calculator/static/media/75.37862a62.svg"
+  },
+  {
+    "revision": "28a215f09ce0146c0b0e22fe2664c536",
+    "url": "/tesla-range-calculator/static/media/90d.28a215f0.svg"
+  },
+  {
+    "revision": "9c937cb94afc2cdccc2cc4cf06dcbfe6",
+    "url": "/tesla-range-calculator/static/media/75d.9c937cb9.svg"
+  },
+  {
+    "revision": "62ed87b41017976b4ffff063c91288cf",
+    "url": "/tesla-range-calculator/static/media/p100d.62ed87b4.svg"
+  },
+  {
+    "revision": "c418a045056d38fac1b7c87898e1506b",
+    "url": "/tesla-range-calculator/static/media/ac-off.c418a045.svg"
+  },
+  {
+    "revision": "4dcccadfb884623189abd5d22d7777fc",
+    "url": "/tesla-range-calculator/static/media/ac-on.4dcccadf.svg"
+  },
+  {
+    "revision": "b3fc73bf1e58ec43c8502bbbc089913c",
+    "url": "/tesla-range-calculator/static/media/up.b3fc73bf.svg"
+  },
+  {
+    "revision": "a56566ba32341898ea4772990a826e86",
+    "url": "/tesla-range-calculator/static/media/down.a56566ba.svg"
+  },
+  {
+    "revision": "987170eab046c4310d198d864f2dc8d7",
+    "url": "/tesla-range-calculator/static/media/heat-off.987170ea.svg"
+  },
+  {
+    "revision": "2c61b1a6023103f04fb44e813e4a9a7b",
+    "url": "/tesla-range-calculator/static/media/heat-on.2c61b1a6.svg"
+  },
+  {
+    "revision": "a52693ae2ceb4b6315fecaf078a0bdbe",
+    "url": "/tesla-range-calculator/static/media/21.a52693ae.svg"
+  },
+  {
+    "revision": "c8ef339847339a7f374dc08857c0413b",
+    "url": "/tesla-range-calculator/static/media/19.c8ef3398.svg"
+  },
+  {
+    "revision": "0c2633d1a77b4c940a585078cf922ef2",
+    "url": "/tesla-range-calculator/index.html"
+  }
+];
